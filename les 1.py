4929ebdf038f2889ced7e87f9b6ea7df6d9393ca
AliceMonte <lol.gerda96@gmail.com>
2022-08-09 00:32:20 +0300

    lesson 5

--- les 1.py
+++ les 1.py
@@ -1,1 +1,22 @@
-rsheshse
+parol=input()
+if parol =='1234':
+    print('Верный пароль')
+else:
+    print ('Введите пароль ещё раз')
+
+    1  # int
+    1.2388  # float
+    "str1"  # str
+    True  # bool
+    Fase  # bool
+    None  # Пустой тип
+    per1 = "hello"
+    per2 = 34
+    per3 = 54.3464
+    per4 = None
+    per5 = True
+
+
+
+
+
