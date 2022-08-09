8c3d9c90e2e5c1d4036250446a453e864688991e
AliceMonte <lol.gerda96@gmail.com>
2022-08-09 22:45:38 +0300

    homework

---
+++ lesson 5.py
@@ -0,0 +1,52 @@
+#1
+i = 5
+print(type(i))
+print (i+1)
+#2
+i = 5
+print(type(i))
+print ('Значение составляет',i)
+#3
+name = 'Дима'
+print(type(name))
+print ('Привет,',name)
+#4
+name = input('Введите имя:')
+print(type(name))
+print ('Привет,',name)
+#5
+a= 2.3
+b= 3.4
+c=a+b
+print(type(a),type(b))
+print('{0:.2}'.format(c))
+#6
+number = 0.5
+print(type(number))
+if number<1:
+    print (True)
+else:
+    print (False)
+#7
+print ('f<d')
+f = int(input('Число f:'))
+d = int (input('Число d:'))
+if f<d :
+    print ('Выражение верное')
+else:
+    print ('Неверно')
+#8
+name = 'Герда'
+running = True
+print(type(running))
+while running:
+    guess = str(input('Угадай моё имя : '))
+    if guess == name:
+        print('Поздравляю, вы угадали.')
+        running = False #
+    else:
+        print('Нет, подумай ещё.')
+else:
+    print('Завершено.')
+#9
+
