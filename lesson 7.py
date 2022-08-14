9aab7f6dff306a19b1f76a97e1a589d491096969
AliceMonte <lol.gerda96@gmail.com>
2022-08-14 22:17:39 +0300

    homework 7

---
+++ lesson 7.py
@@ -0,0 +1,31 @@
+#1
+n1 = 1
+print(n1**100)
+#2
+n2 = 2
+print(n2**16)
+#3
+n3 = 1000
+print(n2-n3)
+n2 -= 1000
+print(n2)
+#4
+n4 = 2+2*2
+print(n4)
+#5
+n5 = 60/3
+print(n5)
+#6
+import math
+n6 = math.sqrt(144)
+print(n6)
+#7
+x = 2+5*14
+print(x, '=x')
+#8
+x = 66-11
+print('x=', x)
+#9
+x = math.sqrt(16-8)
+print(x)
+print(round(x, 2))
