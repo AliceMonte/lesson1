d891f7a4ab6d741ce0dd6aac24e806dac05a94fb
AliceMonte <lol.gerda96@gmail.com>
2022-08-18 21:11:55 +0300

    homework 8

---
+++ lesson 8.py
@@ -0,0 +1,25 @@
+#1
+str1 = "способ 1"
+str2 = 'способ 2'
+#2
+s1 = 'Король'
+s2 = 'Лич'
+print(s1, s2)
+print(s1+' '+s2)
+s3 = s1+' '+s2
+print(s3)
+print((s3+' ')*2)
+#3
+a1 ='Mass'
+a2 ='Effect'
+a3 = 3
+print(a1+' '+a2+' '+str(a3))
+a4 ='Версия'
+a5 = 3.10
+print(a4+' '+str(a5))
+a6 = 'It is'
+a7 = True
+print(a6+' '+str(a7))
+a8 ='А ещё можно - '
+a9 = None
+print(a8+' '+str(a9))
