ed90aea779bbd3ae3911beaffbfcec9bfa3b831d
AliceMonte <lol.gerda96@gmail.com>
2022-08-30 20:09:26 +0300

    homework 12

--- lesson 12.py
+++ lesson 12.py
@@ -6,6 +6,18 @@ while (x):
         m = x % 10
     x //= 10
 print(m)
+
+#1.2
+n = input("Введите число:")
+b = len(n)
+n = int(n)
+Arr = []
+for i in range(b):
+    c = n % 10
+    Arr.append(c)
+    n = n // 10
+print(c)
+
 #2
 a = str(input("Введите число:"))
 print(a[::-1])
