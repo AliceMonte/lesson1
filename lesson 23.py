bf8ffd14a1be5bdb02639a83afef7803d5238c40
AliceMonte <lol.gerda96@gmail.com>
2022-10-15 16:52:23 +0300

    lesson 23

--- lesson 23.py
+++ lesson 23.py
@@ -1,6 +1,7 @@
 #Написать функцию, которая заполняет массив случайными числами в диапазоне, указанном пользователем.
 #Функция должна принимать два аргумента - начало диапазона и его конец, при этом ничего не возвращать.
 #Вывод значений элементов массива должен происходить в основной ветке программы.
+#1
 a = int(input('Введите кол-во элементов: '))
 def addtolist(list1,min,max):
     import random
@@ -9,6 +10,19 @@ def addtolist(list1,min,max):
     list1.sort()
 randomlist = [0] * a
 m = int(input('Введите начало диапазона: '))
-n = int(input('Введите начало диапазона: '))
-addtolist(randomlist,m,n)
+n = int(input('Введите конец диапазона: '))
+addtolist(randomlist, m, n)
 print(randomlist)
+
+#2
+b = int(input('Введите кол-во элементов: '))
+def random(mn,mx):
+    import random
+    return (random.randint(mn, mx))
+list1 = []
+m = int(input('Введите начало диапазона: '))
+n = int(input('Введите конец диапазона: '))
+for i in range(b):
+    list1.append(random(m, n))
+list1.sort()
+print(list1)
